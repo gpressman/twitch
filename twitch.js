f945433079b6ad93007d271a798ddc098fea4621
Gabe Pressman <pressman4747@gmail.com>
2016-04-14 17:41:03 -0400

    now displays link to stream as string

--- twitch.js
+++ twitch.js
@@ -1,10 +1,18 @@
 $(document).ready(function(){
 
+$.getJSON('https://api.twitch.tv/kraken/streams/OgamingSC2?callback=?', function(data) {
+	console.log(data)
 
+});
 
-$.getJSON('https://api.twitch.tv/kraken/users/freecodecamp?callback=?', function(data) {
-  $("#user").html(data.name)
-
+$.getJSON('https://api.twitch.tv/kraken/streams/OgamingSC2?callback=?', function(data) {
+	console.log(data)
+	if (data.stream == null ){
+      $("#user").html("null")
+     }
+     else {
+     	$("#user").html(data._links.self)
+     }
 });
 
 })
