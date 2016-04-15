97d2c8d2bb3c099b137ffbb0ec356f7358e0dd9e
Gabe Pressman <pressman4747@gmail.com>
2016-04-15 17:28:20 -0400

    displays correctly for bad accounts

--- twitch.js
+++ twitch.js
@@ -1,8 +1,10 @@
 $(document).ready(function(){
 
-$.getJSON('https://api.twitch.tv/kraken/streams/OgamingSC2?callback=?', function(data) {
+$.getJSON('https://api.twitch.tv/kraken/streams/comster404?callback=?', function(data) {
 	console.log(data)
-
+    if (data.error == "Unprocessable Entity"){
+    	$("#check").html("Account unavailable")
+    	}
 });
 
 $.getJSON('https://api.twitch.tv/kraken/streams/OgamingSC2?callback=?', function(data) {
