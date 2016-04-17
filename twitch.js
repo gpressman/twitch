527227ec1c6cd4c9c5740843380d383529966e67
Gabe Pressman <pressman4747@gmail.com>
2016-04-17 16:16:20 -0400

    displays list of names and status

--- twitch.js
+++ twitch.js
@@ -1,20 +1,31 @@
 $(document).ready(function(){
-var users = ["freecodecamp", "storbeck", "terakilobyte", "habathcx","RobotCaleb","thomasballinger","noobs2ninjas","beohoff", "OgamingSC2"]
- 
-$(document).ready(function() {
   var users = ["freecodecamp", "storbeck", "terakilobyte", "habathcx", "RobotCaleb", "thomasballinger", "noobs2ninjas", "beohoff", "OgamingSC2"];
-var status = ""
+  var names = ""
+  var status = ""
   users.forEach(function(user) {
-    $.getJSON("https://api.twitch.tv/kraken/streams/" + user + "?callback=?", function(data) {
-    status += "<a href='" + data._links.self + "'>" + user + "</a><br>"
-         
-  $("#data").html(status)
+  	  $.getJSON("https://api.twitch.tv/kraken/streams/" + user + "?callback=?", function(data) {
+  	 if (data.error == "Unprocessable Entity"){
+    	names += "<p>" + user + "</p>"
+    	status += "<p> No user data </p>" 
+    	}
+     else if (data.stream == null ){
+       names += "<p><a href='" + data._links.self + "'>" + user + "</a></p>"
+       status += "<p> offline </p>"
+     }
+     else {
+     	names += "<p><a href='" + data._links.self + "'>" + user + "</a></p>"
+     	status += "<p>" + data.stream.channel.game+ ":" + data.stream.channel.status + "</p>"
+     }
+  
+    $("#names").html(names)    
+    $("#status").html(status)
+})
     })
   })
   
 
-})
-})
+
+
 
 // $.getJSON('https://api.twitch.tv/kraken/streams/comster404?callback=?', function(data) {
 // 	console.log(data)
